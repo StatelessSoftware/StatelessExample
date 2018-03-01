bfae222df2207576303576be15c6017e4e745e92
Drew Immerman <drewimmerman@gmail.com>
2018-03-01 12:31:27 -0500

    [Issue #4] - SCSS should be included by default

--- App/Layout/Head.php
+++ App/Layout/Head.php
@@ -33,9 +33,7 @@ final class Head extends \Stateless\Layout {
             "<!DOCTYPE html><html><head>" .
             "<title>" . $this->title . "</title>" .
             "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" .
-            "<link href=\"https://bootswatch.com/4/pulse/bootstrap.min.css\" " .
-                "rel=\"stylesheet\" type=\"text/css\">" .
-            "<link href=\"/css/style.css\" rel=\"stylesheet\" type=\"text/css\">" .
+            "<link href=\"/css/main.css\" rel=\"stylesheet\" type=\"text/css\">" .
             "<script
                 src=\"https://code.jquery.com/jquery-3.3.1.js\"
                 integrity=\"sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=\"
